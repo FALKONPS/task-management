6ea2bd80a43e68d571bb3fcfab27c3c20541caf0
FALKON.PS <falkonvii@gmail.com>
2024-10-12 01:45:11 +0300

    create app.js with dummy tasks

---
+++ assets/js/app.js
@@ -0,0 +1,37 @@
+const status = {
+  1: 'To Do',
+  2: 'In Progress',
+  3: 'Done',
+};
+
+let tasks = [
+  {
+    id: 1,
+    title: 'Nice',
+    description: 'Search for a job',
+    labels: ['hard'],
+    status: 2,
+  },
+  {
+    id: 2,
+    title: 'Cool',
+    description: 'Find a good movie',
+    labels: ['easy'],
+    status: 1,
+  },
+  {
+    id: 3,
+    title: 'Trip to the Moon',
+    description: 'Build a spaceship and strive to explore space',
+    labels: ['hard'],
+    status: 3,
+  },
+  {
+    id: 4,
+    title: 'Build a Company',
+    description:
+      "No job? Corrupt companies? It's okay, create your own company!",
+    labels: ['easy'],
+    status: 2,
+  },
+];
